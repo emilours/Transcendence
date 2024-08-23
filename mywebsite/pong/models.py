b6714792e86f9ee19af0164804abdddcdc17db43
Malo BURNET <maburnet@e3r8p13.42.fr>
2024-08-23 16:41:10 +0200

    Game model

--- mywebsite/pong/models.py
+++ mywebsite/pong/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+# created at the end of a game
+class Game(models.Model):
+    # users are User model
+    # user1 = 
+    # user2 
+    user1Score = models.IntegerField()
+    user2Score = models.IntegerField()
+    date = models.DateTimeField(auto_now_add=True)
