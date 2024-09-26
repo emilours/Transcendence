9f487b282d232803c6af1270b5332d26ce16f617
Emilie MINATCHY <eminatch@paul-f5Ar2s4.clusters.42paris.fr>
2024-09-26 12:51:19 +0200

    before delete vs code cache

--- mywebsite/authentification/urls.py
+++ mywebsite/authentification/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('signup/', auth_views.signup, name='signup'),
     path('signin/', auth_views.signin, name='signin'),
     path('signout/', auth_views.signout, name='signout'),
-    # path('contactList/', auth_views.contact, name='contactList'),
+    path('contactList/', auth_views.contact, name='contactList'),
     path('send_friend_request/', auth_views.send_friend_request, name='send_friend_request'),
     path('accept_friend_request/<int:friend_request_id>/', auth_views.accept_friend_request, name='accept_friend_request'),
     path('refuse_friend_request/<int:friend_request_id>/', auth_views.refuse_friend_request, name='refuse_friend_request'),
@@ -23,7 +23,7 @@ urlpatterns = [
     path('request_anonymization/', auth_views.request_anonymization, name='request_anonymization'),
 
     # SSE view
-    path('friend-requests/updates/', auth_views.sse_view, name='sse_view'),
+    # path('sse/', auth_views.sse_view, name='sse_view'),
 
     # API URLs last
     path('oauth/complete/42/', api_views.callback_42, name='callback_42'),
